17bf1e4e7375d08f146f70e39ff4bbbbe4e131b0
Colin McParland <colin@tinybird.ca>
2018-01-02 04:18:47 +0000

    Optimized my JS a bit, still mroe to go.  Will eventually seperate it and my SCSS into partials and compile with Mix....Started to build email, just need view and action in route function.

--- public/js/main.js
+++ public/js/main.js
@@ -758,232 +758,252 @@ var googleMapsLoader = __webpack_require__(35);
 var main_map_elem = document.getElementsByClassName('map');
 var preview_map_elem = document.getElementsByClassName('preview-map');
 var googleMapsKey = "AIzaSyDn9LPfEgLCJ0Rwn1nSDdNr-YTQlvuDML0";
+googleMapsLoader.KEY = googleMapsKey;
 var mainmap;
 var previewmap;
 var markers = [];
 
+/**
+ * Function to add pins to a map
+ * @param {Array}   data     Array of marker objects we're to add to the map
+ * @param {boolean} openif   If this bit is set, then present the marker open & center to it
+ */
+function addPins(data, map) {
+  var openif = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
+
+  var address;
+  var raw_address;
+  var review;
+  var date;
+  var rating;
+  var location_id;
+
+  for (var i = 0; i < data.length; i++) {
+
+    raw_address = data[i].location.coordinates;
+
+    address = raw_address.replace(new RegExp(' ', 'g'), '+');
+
+    getLatLong(address, data[i]).then(function (value, curr_data) {
+
+      raw_address = curr_data.location.coordinates;
+      review = curr_data.first_review;
+      date = curr_data.created_at;
+      rating = curr_data.first_rating;
+      location_id = curr_data.location_id;
+
+      address = raw_address.replace(new RegExp(' ', 'g'), '+');
+
+      var latlong = {
+        lat: value.results[0].geometry.location.lat,
+        lng: value.results[0].geometry.location.lng
+      };
+
+      var marker = new google.maps.Marker({
+        position: latlong,
+        title: value.results[0].formatted_address
+      });
+
+      var infowindow = makeInfoWindow(rating, raw_address, date, review, location_id);
+
+      marker.addListener('click', function () {
+        infowindow.open(mainmap, marker);
+      });
+
+      markers.push(marker);
+
+      if (map == 'mainmap') {
+        marker.setMap(mainmap);
+      } else {
+        marker.setMap(previewmap);
+      }
+
+      if (openif == value.results[0].formatted_address) {
+        new google.maps.event.trigger(marker, 'click');
+        mainmap.setCenter(latlong);
+        mainmap.setZoom(15);
+      }
+    });
+  }
+}
+
+/**
+ * Function to build a map, which involves getting all the pins and placing them.
+ * @param  {Object} map    The global variable representing the map
+ * @param  {Object} elem   The HTML element that will hold the map
+ * @param  {Object} center The lat and long of the center
+ * @param  {int} zoom      The desired zoom for the map 
+ * @return {null}        
+ */
+function buildMap(map, elem, center, zoom) {
+
+  /*
+  Set the options for the map we're going to intialize
+   */
+
+  var options = {
+    center: center,
+    zoom: zoom,
+    disableDefaultUI: true,
+    styles: [{ featureType: "road", elementType: "geometry", stylers: [{ lightness: 100 }, { visibility: "simplified" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "visibility": "on" }, { "color": "#C6E2FF" }] }, { "featureType": "poi", "elementType": "geometry.fill", "stylers": [{ "color": "#C5E3BF" }] }, { "featureType": "road", "elementType": "geometry.fill", "stylers": [{ "color": "#D1D1B8" }] }]
+
+    /*
+    Initialize the map
+     */
+
+  };googleMapsLoader.load(function (google) {
+
+    //  Theres gotta be a better way to do this...
+    if (map == 'mainmap') {
+      mainmap = new google.maps.Map(elem, options);
+    } else {
+      previewmap = new google.maps.Map(elem, options);
+    }
+
+    /*
+    If we have a search query, populate the search bar and center the map
+     */
+
+    var args = getURLArgs();
+
+    if (args[0] == 'q') {
+      $('.search-bar').val(urlargs[1].replace(new RegExp('\\+', 'g'), ' '));
+      $('.searchbutton').trigger('click');
+    }
+
+    /*
+    Fetch the pins async, when done place them on the map.
+     */
+
+    $.when(getPins()).then(function (data, textstatus, promise) {
+      addPins(data, map);
+    });
+  });
+}
+
+/**
+ * Function to fetch all the existing pins from the server.
+ * @return {Array} An array of Marker objects
+ */
+function getPins() {
+  return $.ajax({
+    type: 'GET',
+    url: '/api/location',
+    success: function success(msg) {
+      return msg;
+    },
+    error: function error(err) {
+      console.log(err.responseJSON);
+    }
+  });
+}
+
+function getLatLong(address) {
+  var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+  var deferred = $.Deferred();
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + googleMapsKey,
+    success: function success(msg) {
+      deferred.resolve(msg, data);
+    },
+    error: function error(err) {
+      deferred.reject(err);
+    }
+  });
+
+  return deferred.promise();
+}
+
+function makeInfoWindow(rating, raw_address, date, review, location_id) {
+
+  var rating_stars = '<div class="rating-container">';
+
+  //  calculate rating
+  for (var i = 0; i < rating; i++) {
+    rating_stars += '<span class="glyphicon glyphicon-star rating-star"></span>';
+  }
+
+  rating_stars += "</div>";
+
+  var content = '\
+    <h4>Recent Reviews for ' + raw_address.split(',')[0] + '</h4>\
+    <p><strong>Review on ' + date + '</strong></p>\
+    <p>' + review + '</p>\
+    ' + rating_stars + '\
+    <a class="btn btn-default" role="button" href="/location/' + location_id + '" style="font-size: 12px; margin-top: 12px;">See More Reviews</ a>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: content
+  });
+
+  return infowindow;
+}
+
+/**
+ * Function to check for URL arguments, currently only for the 'q' argument
+ * @return {Array} Array of the URL arguments, seperated by '='
+ */
+function getURLArgs() {
+
+  var urlargs = location.href.split('?');
+
+  if (urlargs.length > 1 && $('.map').length) {
+    urlargs = urlargs[1].split('=');
+    return urlargs;
+  } else {
+    return 1;
+  }
+}
+
+/**
+ * Main function
+ */
+
 (function ($) {
   $(document).ready(function () {
 
-    /*
-    Begin Google Maps configuration
-     */
-    googleMapsLoader.KEY = googleMapsKey;
-
     //  Init homepage map
     if ($('.map').length) {
+
       navigator.geolocation.getCurrentPosition(function (position) {
-        googleMapsLoader.load(function (google) {
-          var center = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
 
-          var options = {
-            center: center,
-            zoom: 13,
-            disableDefaultUI: true,
-            styles: [{ featureType: "road", elementType: "geometry", stylers: [{ lightness: 100 }, { visibility: "simplified" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "visibility": "on" }, { "color": "#C6E2FF" }] }, { "featureType": "poi", "elementType": "geometry.fill", "stylers": [{ "color": "#C5E3BF" }] }, { "featureType": "road", "elementType": "geometry.fill", "stylers": [{ "color": "#D1D1B8" }] }]
-          };
+        var center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
 
-          mainmap = new google.maps.Map(main_map_elem[0], options);
-
-          /**
-           * If we have a search query, populate the search bar and center the map
-           */
-
-          var urlargs = location.href.split('?');
-
-          if (urlargs.length > 1 && $('.map').length) {
-            urlargs = urlargs[1].split('=');
-            if (urlargs[0] == 'q') {
-              $('.search-bar').val(urlargs[1].replace(new RegExp('\\+', 'g'), ' '));
-              $('.searchbutton').trigger('click');
-            }
-          }
-
-          $.when(getPins()).then(function (data, textstatus, promise) {
-            addPins(data);
-          });
-        });
+        buildMap('mainmap', main_map_elem[0], center, 13);
       }, function (error) {
 
         showAlert('warning', 'You have geolocation turned off, so we will start you off in Toronto.  You can browse to any city by using the search bar above.');
 
-        googleMapsLoader.load(function (google) {
-          var center = {
-            lat: 43.6532,
-            lng: -79.3832
-          };
+        var center = {
+          lat: 43.6532,
+          lng: -79.3832
+        };
 
-          var options = {
-            center: center,
-            zoom: 13,
-            disableDefaultUI: true,
-            styles: [{ featureType: "road", elementType: "geometry", stylers: [{ lightness: 100 }, { visibility: "simplified" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "visibility": "on" }, { "color": "#C6E2FF" }] }, { "featureType": "poi", "elementType": "geometry.fill", "stylers": [{ "color": "#C5E3BF" }] }, { "featureType": "road", "elementType": "geometry.fill", "stylers": [{ "color": "#D1D1B8" }] }]
-          };
-
-          mainmap = new google.maps.Map(main_map_elem[0], options);
-
-          /**
-           * If we have a search query, populate the search bar and center the map
-           */
-
-          var urlargs = location.href.split('?');
-
-          if (urlargs.length > 1 && $('.map').length) {
-            urlargs = urlargs[1].split('=');
-            if (urlargs[0] == 'q') {
-              $('.search-bar').val(urlargs[1].replace(new RegExp('\\+', 'g'), ' '));
-              $('.searchbutton').trigger('click');
-            }
-          }
-
-          $.when(getPins()).then(function (data, textstatus, promise) {
-            addPins(data);
-          });
-        });
+        buildMap('mainmap', main_map_elem[0], center, 13);
       });
     }
 
     //  Init locations page map
-
     if ($('.preview-map').length) {
-      googleMapsLoader.load(function (google) {
-        var addr = $('.addr').text().replace(new RegExp(' ', 'g'), '+');
 
-        getLatLong(addr).then(function (msg) {
-          var center = { lat: msg.results[0].geometry.location.lat, lng: msg.results[0].geometry.location.lng };
+      var addr = $('.addr').text().replace(new RegExp(' ', 'g'), '+');
 
-          var options = {
-            center: center,
-            zoom: 15,
-            disableDefaultUI: true,
-            styles: [{ featureType: "road", elementType: "geometry", stylers: [{ lightness: 100 }, { visibility: "simplified" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "visibility": "on" }, { "color": "#C6E2FF" }] }, { "featureType": "poi", "elementType": "geometry.fill", "stylers": [{ "color": "#C5E3BF" }] }, { "featureType": "road", "elementType": "geometry.fill", "stylers": [{ "color": "#D1D1B8" }] }],
-            gestureHandling: 'none',
-            zoomControl: false
-          };
+      getLatLong(addr).then(function (msg) {
 
-          previewmap = new google.maps.Map(preview_map_elem[0], options);
+        var center = {
+          lat: msg.results[0].geometry.location.lat,
+          lng: msg.results[0].geometry.location.lng
+        };
 
-          var marker = new google.maps.Marker({
-            position: center
-          });
-
-          marker.setMap(previewmap);
-        });
+        buildMap('previewmap', preview_map_elem[0], center, 15);
       });
-    }
-
-    /*
-    End Google Maps config
-     */
-
-    /**
-     * Get all the pins and place them on the map.
-     */
-
-    function getPins() {
-      return $.ajax({
-        type: 'GET',
-        url: '/api/location',
-        success: function success(msg) {
-          return msg;
-        },
-        error: function error(err) {
-          console.log(err.responseJSON);
-        }
-      });
-    }
-
-    function getLatLong(address) {
-      var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-
-      var deferred = $.Deferred();
-
-      $.ajax({
-        type: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + googleMapsKey,
-        success: function success(msg) {
-          console;
-          deferred.resolve(msg, data);
-        },
-        error: function error(err) {
-          deferred.reject(err);
-        }
-      });
-
-      return deferred.promise();
-    }
-
-    function addPins(data) {
-      var openif = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-
-
-      var address;
-      var raw_address;
-      var review;
-      var date;
-      var rating;
-      var location_id;
-
-      for (var i = 0; i < data.length; i++) {
-        raw_address = data[i].location.coordinates;
-        address = raw_address.replace(new RegExp(' ', 'g'), '+');
-
-        getLatLong(address, data[i]).then(function (value, curr_data) {
-
-          raw_address = curr_data.location.coordinates;
-          review = curr_data.first_review;
-          date = curr_data.created_at;
-          rating = curr_data.first_rating;
-          location_id = curr_data.location_id;
-          address = raw_address.replace(new RegExp(' ', 'g'), '+');
-
-          var latlong = { lat: value.results[0].geometry.location.lat, lng: value.results[0].geometry.location.lng };
-
-          console.log(latlong);
-
-          var marker = new google.maps.Marker({
-            position: latlong,
-            title: value.results[0].formatted_address
-          });
-
-          var rating_stars = '<div class="rating-container">';
-
-          //  calculate rating
-          for (var i = 0; i < rating; i++) {
-            rating_stars += '<span class="glyphicon glyphicon-star rating-star"></span>';
-          }
-
-          rating_stars += "</div>";
-
-          var content = '\
-            <h4>Recent Reviews for ' + raw_address.split(',')[0] + '</h4>\
-            <p><strong>Review on ' + date + '</strong></p>\
-            <p>' + review + '</p>\
-            ' + rating_stars + '\
-            <a class="btn btn-default" role="button" href="/location/' + location_id + '" style="font-size: 12px; margin-top: 12px;">See More Reviews</a>';
-
-          var infowindow = new google.maps.InfoWindow({
-            content: content
-          });
-
-          marker.addListener('click', function () {
-            infowindow.open(mainmap, marker);
-          });
-
-          markers.push(marker);
-
-          marker.setMap(mainmap);
-
-          if (openif == value.results[0].formatted_address) {
-            new google.maps.event.trigger(marker, 'click');
-            mainmap.setCenter(latlong);
-            mainmap.setZoom(15);
-          }
-        });
-      }
     }
 
     /**
@@ -1062,7 +1082,7 @@ var markers = [];
                     //  Empty the marker array
                     markers = [];
 
-                    addPins(data, addr);
+                    addPins(data, 'mainmap', addr);
 
                     $('#newReviewModal').modal('hide');
 
